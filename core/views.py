9b4bb27ff7825a251569911e7de89aab852c0d09
Syarif Ys <5yarif.y.s@gmail.com>
2022-10-10 19:31:51 +0800

    5 Oktober 2022 v0.3
    10 Oktober 2022
    v0.5
    error again

--- core/views.py
+++ core/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import auth, messages
 
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def signup(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        print(username)
+    else:
+        return render(request, 'signup.html')
+
+def signin(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('signin')
+    else:
+        return render(request, 'signin.html')
